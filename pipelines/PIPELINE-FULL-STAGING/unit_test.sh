c9fa00f8e4ee8ccbf7ed4da9abe99077d7147d28
Ubuntu <ubuntu@ip-172-31-86-145.ec2.internal>
2022-01-10 22:25:33 +0000

    Add ENDPOIT_OVERRIDE in unit_test.sh file

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -5,6 +5,9 @@ set -x
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 echo "PYTHONPATH: $PYTHONPATH"
 export DYNAMODB_TABLE=todoUnitTestsTable
+export ENDPOINT_OVERRIDE=http://localhost:8000
+echo "ENDPOINT_OVERRIDE"  
+printenv ENDPOINT_OVERRIDE
 python test/unit/TestToDo.py
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py
